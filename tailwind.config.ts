a169b822b650c20ee18ee5cf2113a35997149cd1
Mahendradeokar <deokarmahendra424@gmail.com>
2025-06-12 00:47:33 +0530

    Chore

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,9 @@ export default {
         md: "calc(var(--radius) - 2px)",
         sm: "calc(var(--radius) - 4px)",
       },
+      borderColor: {
+        borderColor: "var(--border)",
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
